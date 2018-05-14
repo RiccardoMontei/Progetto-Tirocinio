de78722af98d152b1a46ac205887d07e8f40169d
RiccardoMontei <riccardo.montei@outlook.com>
2018-05-14 12:49:48 +0200

    Crafting System
    Inserito crafting system e ML agent

---
+++ Mad Moves Zombie IA/Assets/GameAssets/Player/CraftingSystem/CraftingSystem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingSystem : MonoBehaviour {
+	public GameObject fence;
+	public GameObject pivotFence;
+	public bool activeCrafting = false;
+
+	public int wood=0;
+	public int rope=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartStopCrafting ();	
+
+		if (activeCrafting) {
+			if (Input.GetButton ("Fire1"))
+				pivotFence.transform.Rotate (0, 4,0);
+			if(Input.GetButton("Aim"))
+				pivotFence.transform.Rotate (0, -4,0);
+
+			RaycastHit hit;
+			Debug.DrawRay(transform.position, transform.forward * 7, Color.magenta);
+
+			if (Physics.Raycast(transform.position, transform.forward, out hit, 7))
+			{//Se il raggio colpisce qualcosa!
+				Debug.Log (hit.transform.gameObject.tag);
+				if (hit.transform.gameObject.CompareTag("Ground")) 
+				{
+					if (Input.GetKeyDown (KeyCode.E))
+					if (wood >= 6 && rope >= 2) {
+						Instantiate (fence, pivotFence.transform.position, pivotFence.transform.rotation);
+						wood -= 6;
+						rope -= 2;
+					}
+				}
+
+			}
+		}
+	}
+
+	public void StartStopCrafting(){
+		
+		if (Input.GetKeyDown (KeyCode.T)) {
+			if (!activeCrafting && !pivotFence.activeInHierarchy) {
+				activeCrafting = true;
+				pivotFence.SetActive (true);
+			} else if (activeCrafting && pivotFence.activeInHierarchy) {
+				activeCrafting = false;
+				pivotFence.SetActive (false);
+			}
+		}
+	}
+}
---
+++ Mad Moves Zombie IA/Assets/GameAssets/Player/Scripts/PlayerScripts/AimCamera.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+
+public class AimCamera : MonoBehaviour {
+
+	public GameObject theCamera;
+	public GameObject player;
+	public GameObject wP;
+
+	private Vector3 cameraRotation;
+	private Transform cameraPosition;
+
+	private Camera cameraOBJ;
+
+	private FirstPersonController controller;
+
+	private bool isAiming;
+	public Vector3 pointOfViewAimWeapon;
+	public float fieldOfViewAimWeapon=0;
+
+	// Use this for initialization
+	void Start () {
+		cameraOBJ=Camera.main;
+		cameraPosition = theCamera.GetComponent<Transform> ();
+		controller = player.GetComponent<FirstPersonController> ();//Prendo il FPC del player su controller
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		pointOfViewAimWeapon = wP.GetComponent<WeaponsManager>().YourWeapon().GetComponent<WeaponsDettails>() .pointOfViewAimWeapon;
+		fieldOfViewAimWeapon = wP.GetComponent<WeaponsManager>().YourWeapon().GetComponent<WeaponsDettails>().fieldOfViewAimWeapon;
+
+		cameraPosition = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Transform> (); 
+		cameraRotation = cameraPosition.rotation.eulerAngles;
+		cameraRotation = new Vector3 (-cameraRotation.x, cameraRotation.y + 180, cameraRotation.z); //Variabile che contiene la rotazione della camera modificata ad hoc per il nostro player
+
+		transform.rotation = Quaternion.Euler (cameraRotation);//Ruoto sull'asse verticale il body del player in base al movimento della camera
+		// if per forzare il fieldOfView della camera che crea problemi mentre si corre
+		if(cameraOBJ.fieldOfView!=53.0f && !isAiming) 
+			cameraOBJ.fieldOfView = 53.0f;
+
+		if (cameraOBJ.fieldOfView != fieldOfViewAimWeapon && isAiming)
+			cameraOBJ.fieldOfView = fieldOfViewAimWeapon;
+
+		/* Funzioni per la mira il valore di spostamento cambia in base all'arma che si impugna*/
+		if (cameraOBJ.fieldOfView == 53.0f) { 
+			if (!wP.GetComponent<WeaponsManager> ().animator.GetBool ("BaseballBat") && !wP.GetComponent<WeaponsManager> ().animator.GetBool ("Machete")) {
+				if (Input.GetButtonDown ("Aim") && !GameObject.FindGameObjectWithTag ("CraftingSystem").GetComponent<CraftingSystem> ().activeCrafting) {
+					isAiming = true;
+					controller.m_RunSpeed = 5.0f;//Quando si mira non si corre, classico degli FPS
+					cameraOBJ.fieldOfView = fieldOfViewAimWeapon;
+					transform.localPosition = pointOfViewAimWeapon;
+				}
+			}
+		}
+
+		if (Input.GetButtonUp ("Aim")) {
+			//Torno alle impostazioni di default
+			cameraOBJ.fieldOfView = 53.0f;
+			transform.localPosition = new Vector3 (0.07f, -0.2f, -0.13f); //posizione di default
+			controller.m_RunSpeed = 10.0f;
+			isAiming=false;
+		}
+
+		/**************************************************************************************************/
+
+	}
+}
